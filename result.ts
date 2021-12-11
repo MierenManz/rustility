13de397f904a573358ca4908985b9462feb96cb6
MierenManz <christiaanmens@gmail.com>
2021-12-11 22:26:08 +0100

    changes

--- result.ts
+++ result.ts
@@ -5,20 +5,27 @@ export interface Result<Ok, Err> {
   isOk(): boolean;
 }
 
+const UNWRAP_ERROR_ERROR = new Error("Tried to unwrap error when value was Ok");
+const UNWRAP_ERROR = new Error("Tried to unwrap value when value was Error");
+
 export function Ok<Ok, Err>(val: Ok): Result<Ok, Err> {
   return {
     unwrap: () => val,
-    unwrapErr: () => { throw new Error("Tried to unwrap error when value was ok") },
+    unwrapErr: () => {
+      throw UNWRAP_ERROR_ERROR;
+    },
     isErr: () => false,
     isOk: () => true,
-  }
+  };
 }
 
 export function Err<Ok, Err>(val: Err): Result<Ok, Err> {
   return {
-    unwrap: () => { throw new Error("Tried to unwrap value when value was err") },
+    unwrap: () => {
+      throw UNWRAP_ERROR;
+    },
     unwrapErr: () => val,
     isErr: () => true,
     isOk: () => false,
-  }
+  };
 }
