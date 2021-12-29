6c5f911a46bd6522325f0587754ca49b5d494908
MierenManz <christiaanmens@gmail.com>
2021-12-29 21:15:14 +0100

    intergrate unwrap tests into other tests

--- option_test.ts
+++ option_test.ts
@@ -2,6 +2,7 @@ import { None, Some } from "./option.ts";
 import {
   assertEquals,
   assertThrows,
+  assert
 } from "https://deno.land/std@0.115.1/testing/asserts.ts";
 
 Deno.test({
@@ -10,6 +11,8 @@ Deno.test({
     const res = Some("Some string");
 
     assertEquals(res.unwrap(), "Some string");
+    assertEquals(res.unwrapOr("not some"), "Some string");
+    assertEquals(res.unwrapOrElse(() => "not some"), "Some string");
     assertEquals(res.isSome(), true);
     assertEquals(res.isNone(), false);
   },
@@ -20,16 +23,10 @@ Deno.test({
   fn: function () {
     const res = None;
 
+    assertThrows(res.unwrap);
+    assertEquals(res.unwrapOr("not none"), "not none");
+    assertEquals(res.unwrapOrElse(() => "not none"), "not none");
     assertEquals(res.isSome(), false);
     assertEquals(res.isNone(), true);
-  },
-});
-
-Deno.test({
-  name: "unwraps",
-  fn: function () {
-    const noneValue = None;
-
-    assertThrows(noneValue.unwrap);
   },
 });
